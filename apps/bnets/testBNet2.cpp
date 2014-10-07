750d7ad4cba3020d12f83398fed177b7a0982278
breno <breno@breno-machine.(none)>
2014-10-07 09:43:50 -0300

    Adding information to perform evidence

--- apps/bnets/testBNet2.cpp
+++ apps/bnets/testBNet2.cpp
@@ -2,8 +2,8 @@
 *  \author: Breno Carvalho
 */
 
-//#ifndef BC_BAYESIAN_NETWORK_HPP
-//#define BC_BAYESIAN_NETWORK_HPP
+#ifndef BC_BAYESIAN_NETWORK_HPP
+#define BC_BAYESIAN_NETWORK_HPP
 
 // #include <cstdlib>
 // #include <cassert>
@@ -51,11 +51,15 @@ public:
 
     void add_variable(std::vector<std::string> values, std::vector<double> cpd, std::string variable_id){
         add_variable(values, variable_id);
-    //    variables[variable_id].set_cpd(dense_table_t(cpd));
+        variables[variable_id].set_cpd(cpd);
+    //hasChanged = true;
     };
     void add_variable(std::vector<std::string> values, std::string variable_id){
         Variable<MAX_DIM> var(values, variable_id);
-        variables[variable_id] = var;
+        variables[variable_id] = var;std::cout << values.size();
+        variable_t f_g_var = fgraph.add_variable(values.size(), variable_id);
+        var.set_f_graph_node(f_g_var);
+        hasChanged = true;
     };
 
     void set_variable_parents(std::vector<std::string> parent_ids, std::string variable_id){
@@ -64,14 +68,20 @@ public:
             parents[i] = variables[parent_ids[i]];
         }
         variables[variable_id].set_parents(parents);
+        hasChanged = true;
     };
 
     void add_variable_parent(std::string parent_id, std::string variable_id){
         variables[variable_id].add_parent(variables[parent_id]);
+        hasChanged = true;
     };
-    void set_variable_cpd(std::vector<double> cpd, std::string id){};
-    void set_variable_cpd(dense_table_t cpd, std::string id){};
-    dense_table_t get_variable_cpd(variable_t id){return NULL;};
+    void set_variable_cpd(std::vector<double> cpd, std::string id){
+        variables[id].set_cpd(cpd);
+    };
+    //void set_variable_cpd(dense_table_t cpd, std::string id){};
+    dense_table_t get_variable_cpd(variable_t id){
+        return variables[id].get_cpd();
+    };
 
     void set_evidence(std::map<std::string, std::string> evidence){
         this->evidence(evidence);
@@ -79,20 +89,44 @@ public:
         for(; iterator != evidence.end(); iterator++){
             variables[(*iterator).first].set_evidence((*iterator).second);
         }
+        hasChanged = true;
     };
     void add_evidence(std::string id, std::string value){
         evidence[id] = value;
         variables[id].set_evidence(value);
+        hasChanged = true;
     };
-    void clear_evidence(){};
+
+    void clear_evidence(std::string id){
+        variables[id].clear_evidence();
+        evidence.erase(id);
+        hasChanged = true;
+    }
+    void clear_evidence(){
+        std::map<std::string, std::string >::const_iterator iter = evidence.begin();
+        for(; iter != evidence.end(); iter++){
+            variables[(*iter).first].clear_evidence();
+        }
+        evidence.clear();
+        hasChanged = true;
+    };
     std::string get_evidence(std::string variable_id){
         return evidence;
     };
     std::map<std::string, std::string> get_evidence();
 
-    dense_table_t infer_variable_cpd(InferenceEngine eng, std::string variable_id){dense_table_t d; return d;};
-    dense_table_t infer_variables_cpd(InferenceEngine eng, std::vector<std::string> variable_ids){dense_table_t d; return d;};
-    dense_table_t infer_all_variables_cpd(InferenceEngine eng){dense_table_t d; return d;};
+    dense_table_t infer_variable_cpd(InferenceEngine eng, std::string variable_id){
+        dense_table_t d; return d;
+        hasChanged = false;
+        };
+    dense_table_t infer_variables_cpd(InferenceEngine eng, std::vector<std::string> variable_ids){
+        dense_table_t d;
+        hasChanged = false;
+        return d;};
+    dense_table_t infer_all_variables_cpd(InferenceEngine eng){
+        dense_table_t d;
+        hasChanged = false;
+        return d;};
 
     //Use SFrames to get info to set your parameters (complete data)
     //Use SFrames to get info to set your parameters (incomplete data) <- Use an engine
@@ -124,40 +158,53 @@ private:
     typename BeliefNetwork<MAX_DIM>::dense_table_t distribution;
     typename BeliefNetwork<MAX_DIM>::variable_t f_g_var;
     std::vector<Variable> parents;
+    std::vector<typename BeliefNetwork<MAX_DIM>::variable_t> f_parents;
     std::vector<std::string> values;
     int evidence;
     std::string id;
 
 public:
-    Variable(std::vector<std::string> values, std::string variable_id): values(values), id(variable_id), evidence(-1){}
+    Variable(std::vector<std::string> values, std::string variable_id):
+            values(values), id(variable_id), evidence(-1){};
     Variable():id(""), evidence(-1){};
 
     std::string get_id(){ return id;}
 
     void set_parents(std::vector<Variable> parents){
         this->parents.clear();
+        f_parents.clear();
         for(int i = 0; i < parents.size(); i++){
             this->parents.push_back(parents[i]);
+            f_parents.push_back(parents[i].get_f_graph_node());
         }
-        distribution = typename BeliefNetwork<MAX_DIM>::dense_table_t();
     };
 
     void add_parent(Variable parent){
         parents.push_back(parent);
+        f_parents.push_back(parent.get_f_graph_node());
     };
 
-    //void set_cpd(std::vector<double> cpd, std::string id){};
-    void set_cpd(typename BeliefNetwork<MAX_DIM>::dense_table_t cpd){
-    //    distribution = cpd;
+    void set_f_graph_node(typename BeliefNetwork<MAX_DIM>::variable_t node){f_g_var = node;}
+    typename BeliefNetwork<MAX_DIM>::variable_t get_f_graph_node(){return f_g_var;}
+
+    void set_cpd(std::vector<double> cpd){
+        f_parents.push_back(f_g_var);
+        //typename BeliefNetwork<MAX_DIM>::dense_table_t tmp(f_parents, cpd);
+        //distribution = tmp;
+        f_parents.pop_back();
     };
-    //dense_table_t get_cpd(variable_t id){return NULL;};
+
+    typename BeliefNetwork<MAX_DIM>::dense_table_t get_cpd(){return distribution;};
+
+    void clear_evidence(){ evidence = -1;}
 
     void set_evidence(std::string value){
         int i = 0;
         for(; i < values.size(); i++){
-            if(!values[i].compare(value)){ // if we have a match with a value in the values list then:
+            if(!values[i].compare(value)){ // if we have a match of value in the values list then:
                 evidence = i;
                 //TODO update the factor graph
+                //distribution.set_logP(index, value);
             }
         }
     }
@@ -199,7 +246,11 @@ public:
 
 const size_t MAX_DIM1 = 10;
 
-int main(){
+int main(int argc, char** argv){
+    graphlab::mpi_tools::init(argc, argv);
+    // Create a distributed control object (must come after mpi_tools::init())
+    graphlab::distributed_control dc;
+
     //Initiates a Bayesian Network
     BeliefNetwork<MAX_DIM1> bnet;
 
@@ -208,25 +259,25 @@ int main(){
     values[0] = "true"; values[1] = "false";
 
     //Criate first variable, rain
-    std::vector<double> cpd_r(4, 0.0);
-    cpd_r[0] = 0.4; cpd_r[1] = 0.6;
+    std::vector<double> cpd_r(2, 0.0);
+    cpd_r[0] = log(0.4); cpd_r[1] = log(0.6);
     bnet.add_variable(values, cpd_r, "rain");
 
     //Criate second variable, wet
     std::vector<double> cpd_w(4, 0.0);
     bnet.add_variable(values, "wet");
     bnet.add_variable_parent("rain", "wet");
-    cpd_w[0] = 0.9; cpd_w[2] = 0.1;
-    cpd_w[1] = 0.4; cpd_w[3] = 0.6;
+    cpd_w[0] = log(0.9); cpd_w[2] = log(0.1);
+    cpd_w[1] = log(0.4); cpd_w[3] = log(0.6);
     bnet.set_variable_cpd(cpd_w, "wet");
 
     //Run inference
     InferenceEngine eng;
-    bnet.infer_variable_cpd(eng, "wet");
     bnet.add_evidence("rain", "true");
+    bnet.infer_variable_cpd(eng, "wet");
     std::cout << 0.0 << std::endl;
 
     std::cout << bnet<< std::endl;
 };
 
-//#endif //BC_BAYESIAN_NETWORK_HPP
+#endif //BC_BAYESIAN_NETWORK_HPP
